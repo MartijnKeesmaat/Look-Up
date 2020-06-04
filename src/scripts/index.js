5963c275f8a7278d76f56c45861cdb2db3b8eb44
Martijn <mpkeesmaat@gmail.com>
2020-06-04 17:22:18 +0200

    add howler + transitions

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,4 @@
+import { Howl, Howler } from 'howler';
 import '../styles/index.sass';
 import './pace.min.js';
 import gsap from 'gsap';
@@ -67,8 +68,15 @@ barba.init({
   ],
 });
 
+// var sound = new Howl({
+//   src: ['assets/sound/podcast.mp3'],
+// });
+
+// sound.play();
+
 const $container = document.querySelector('.scroll-container');
 const $pause = document.querySelector('.pause');
+const $ch = document.querySelectorAll('.ch');
 
 let scrollIsPaused = false;
 
@@ -76,11 +84,13 @@ const containerHeigth = $container.offsetHeight;
 const speed = 3.5;
 const tl = gsap.timeline();
 
+// Scroll container to bottom
 tl.to('.scroll-container', {
   y: -containerHeigth,
   duration: containerHeigth / (speed * 10),
 });
 
+// Pause/Resume scroll
 $pause.addEventListener('click', function (e) {
   if (scrollIsPaused) {
     tl.play();
@@ -91,3 +101,48 @@ $pause.addEventListener('click', function (e) {
   }
   scrollIsPaused = !scrollIsPaused;
 });
+
+let chapterStatus = [];
+$ch.forEach((e) => {
+  chapterStatus.push([false, false, false]);
+});
+
+// Check if each chapter is currently on screen
+let checkStatusInterval = setInterval(() => {
+  $ch.forEach((chapter, i) => {
+    const y = chapter.getBoundingClientRect().y;
+    const bottom = 450;
+    const top = 30;
+
+    if (!chapterStatus[i][0]) {
+      if (y < bottom && y > top) {
+        // Do something
+        console.log(chapter.dataset.ch + ' is on screen');
+        chapter.classList.add('showmebro');
+
+        // clear status
+        chapterStatus[i][0] = !chapterStatus[i][0];
+      }
+    }
+
+    if (!chapterStatus[i][1]) {
+      if (y < top) {
+        // Do something
+        console.log(chapter.dataset.ch + ' has left');
+
+        // clear status
+        chapterStatus[i][1] = !chapterStatus[i][1];
+      }
+    }
+
+    if (!chapterStatus[i][2]) {
+      if (y > bottom) {
+        // Do something
+        console.log(chapter.dataset.ch + ' has not entered');
+
+        // clear status
+        chapterStatus[i][2] = !chapterStatus[i][2];
+      }
+    }
+  });
+}, 500);
