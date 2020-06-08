50e5a3c354c88f496356b314e441eeb0d8d0ab1c
Martijn <mpkeesmaat@gmail.com>
2020-06-08 22:50:48 +0200

    did some stuff here

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -73,76 +73,124 @@ barba.init({
 // });
 
 // sound.play();
+function timestampToMilliSeconds(ms) {
+  var a = ms.split(':');
+  return (Number(a[0]) * 60 + Number(a[1])) * 1000;
+}
 
-const $container = document.querySelector('.scroll-container');
-const $pause = document.querySelector('.pause');
-const $ch = document.querySelectorAll('.ch');
+const timestamps = [
+  ['piano', '0:00', '0:23'],
+  ['intro', '0:24', '2:07'],
+];
+const sprites = [];
 
-let scrollIsPaused = false;
+timestamps.forEach((e) => {
+  const key = e[0];
+  const start = timestampToMilliSeconds(e[1]);
+  const duration = timestampToMilliSeconds(e[2]) - timestampToMilliSeconds(e[1]);
 
-const containerHeigth = $container.offsetHeight;
-const speed = 3.5;
-const tl = gsap.timeline();
-
-// Scroll container to bottom
-tl.to('.scroll-container', {
-  y: -containerHeigth,
-  duration: containerHeigth / (speed * 10),
+  sprites.push([start, duration]);
 });
 
-// Pause/Resume scroll
-$pause.addEventListener('click', function (e) {
-  if (scrollIsPaused) {
-    tl.play();
-    e.currentTarget.innerHTML = 'pause';
-  } else {
-    tl.pause();
-    e.currentTarget.innerHTML = 'play';
-  }
-  scrollIsPaused = !scrollIsPaused;
+// console.log(seconds);
+
+console.log(sprites);
+
+var sound = new Howl({
+  src: ['assets/sound/podcast.mp3'],
+  sprite: {
+    piano: sprites[0],
+    intro: sprites[1],
+  },
 });
 
-let chapterStatus = [];
-$ch.forEach((e) => {
-  chapterStatus.push([false, false, false]);
+// new Howl({
+//   sprite: {
+//     piano: [offset, duration, (loop)]
+//   },
+// });
+
+// console.log(sound._sprite);
+
+sound.once('load', function () {
+  // sound.play('intro');
+  // sound.rate(1.5);
+  tl.play();
+
+  console.log(sound.duration());
 });
 
-// Check if each chapter is currently on screen
-let checkStatusInterval = setInterval(() => {
-  $ch.forEach((chapter, i) => {
-    const y = chapter.getBoundingClientRect().y;
-    const bottom = 450;
-    const top = 30;
+// const $container = document.querySelector('.scroll-container');
+// const $pause = document.querySelector('.pause');
+// const $ch = document.querySelectorAll('.ch');
 
-    if (!chapterStatus[i][0]) {
-      if (y < bottom && y > top) {
-        // Do something
-        console.log(chapter.dataset.ch + ' is on screen');
-        chapter.classList.add('showmebro');
+// let scrollIsPaused = false;
 
-        // clear status
-        chapterStatus[i][0] = !chapterStatus[i][0];
-      }
-    }
+// const containerHeigth = $container.offsetHeight;
+// const speed = 3.5;
+// const tl = gsap.timeline();
 
-    if (!chapterStatus[i][1]) {
-      if (y < top) {
-        // Do something
-        console.log(chapter.dataset.ch + ' has left');
+// // Scroll container to bottom
+// tl.to('.scroll-container', {
+//   y: -containerHeigth,
+//   duration: containerHeigth / (speed * 10),
+// });
 
-        // clear status
-        chapterStatus[i][1] = !chapterStatus[i][1];
-      }
-    }
+// tl.pause();
 
-    if (!chapterStatus[i][2]) {
-      if (y > bottom) {
-        // Do something
-        console.log(chapter.dataset.ch + ' has not entered');
+// // Pause/Resume scroll
+// $pause.addEventListener('click', function (e) {
+//   if (scrollIsPaused) {
+//     tl.play();
+//     e.currentTarget.innerHTML = 'pause';
+//   } else {
+//     tl.pause();
+//     e.currentTarget.innerHTML = 'play';
+//   }
+//   scrollIsPaused = !scrollIsPaused;
+// });
 
-        // clear status
-        chapterStatus[i][2] = !chapterStatus[i][2];
-      }
-    }
-  });
-}, 500);
+// let chapterStatus = [];
+// $ch.forEach((e) => {
+//   chapterStatus.push([false, false, false]);
+// });
+
+// // Check if each chapter is currently on screen
+// let checkStatusInterval = setInterval(() => {
+//   $ch.forEach((chapter, i) => {
+//     const y = chapter.getBoundingClientRect().y;
+//     const bottom = 450;
+//     const top = 30;
+
+//     if (!chapterStatus[i][0]) {
+//       if (y < bottom && y > top) {
+//         // Do something
+//         console.log(chapter.dataset.ch + ' is on screen');
+//         chapter.classList.add('showmebro');
+
+//         // clear status
+//         chapterStatus[i][0] = !chapterStatus[i][0];
+//       }
+//     }
+
+//     if (!chapterStatus[i][1]) {
+//       if (y < top) {
+//         // Do something
+//         console.log(chapter.dataset.ch + ' has left');
+
+//         // clear status
+//         chapterStatus[i][1] = !chapterStatus[i][1];
+//       }
+//     }
+
+//     if (!chapterStatus[i][2]) {
+//       if (y > bottom) {
+//         // Do something
+//         console.log(chapter.dataset.ch + ' has not entered');
+
+//         // clear status
+//         chapterStatus[i][2] = !chapterStatus[i][2];
+//       }
+//     }
+//   });
+// }, 500);
